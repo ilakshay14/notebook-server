3c605ebcd5e0d4de7ddb6b3d367fd71fe86f1f81
Lakshay Malhotra <lakshay@Lakshays-MacBook-Pro.local>
2020-12-09 00:37:53 +0530

    added user reg and login with validations

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config');
-
-const typeDefs = gql`
-    type Query{
-        sayHi: String!
-    }
-`;
-
-const resolvers = {
-    Query: {
-        sayHi: () => `Hello World!`
-    }
-
-}
 
 const SERVER = new ApolloServer({
     typeDefs,
@@ -22,5 +11,8 @@ const SERVER = new ApolloServer({
 });
 
 mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => SERVER.listen({ port: 5000 }))
-    .then((res) => console.log(`Server running on ${res.url}`));
+    .then(() => {
+        console.log('Mongi DB connected');
+        SERVER.listen({ port: 5000 })
+        .then((res) => console.log(`Server running on ${res.url}`));
+    })
